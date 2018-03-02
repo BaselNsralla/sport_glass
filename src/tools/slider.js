654833ea9ede18694adb843abf48c12ee08d78c6
Basel <basel.obeid9661@gmail.com>
2018-03-02 00:28:24 -0800

    slider with tints

--- src/tools/slider.js
+++ src/tools/slider.js
@@ -1,13 +1,23 @@
+
 var $  = require('jquery')
 class Slider {
-  constructor(wrapper) {
+  constructor(wrapper, start = 0) {
+    this.current = start 
     this.wrapper = wrapper
+    this.tints = []
   }
 
   init () {
-    this.current = 0
     this.sheets = $(`${this.wrapper}`).children('div').hide()
+    let slider = document.createElement('div')
+    slider.className = 'slider'
+    $(`${this.wrapper}`).empty()
+    $(`${this.wrapper}`).append(slider)
+    $('.slider').append(this.sheets)
+    $('.slider').css('height',$(`${this.wrapper}`).css('height'))
+    this.createTints(this.sheets.length)
     $(this.sheets[this.current]).show()
+    this.blurInactiveTint(this.current)
     setInterval(()=>this.setActive((this.current+1)%this.sheets.length), 4000)
   }
 
@@ -15,15 +25,56 @@ class Slider {
     console.log('rere')
   }
 
+  createTints(count) {
+    let tintFather = document.createElement('div')
+    tintFather.className = 'tint-container'
+    for(var i = 0; i < count; i++) {
+      let tint =  document.createElement('div')
+      tint.className = 'tint'
+      this.tints[i] = tint
+      tintFather.appendChild(tint)
+    }
+    $(`${this.wrapper}`).append(tintFather)
+    this.buildCSS()    
+  }
+
   setActive(index) {
     console.log(index)
     this.current = (this.current + 1)%this.sheets.length
+    this.blurInactiveTint(index)
     this.sheets.map((i, element) => {
       if (index === i ) {
-        $(this.sheets[i]).show()
+        setTimeout(() => $(this.sheets[i]).slideDown(), 300)
       } else {
-        $(this.sheets[i]).hide()
+        $(this.sheets[i]).slideUp()
       }
+    })
+  }
+
+  blurInactiveTint(index) {
+   this.tints.map((tint, i)=> {
+     if(i === index) {
+       $(tint).css('background-color', 'grey')
+     } else {
+      $(tint).css('background-color', 'white')
+     }
+   })
+  }
+
+  buildCSS () {
+    $('.tint-container').css({
+      'width': '275px',
+      'height': '50px',
+      'background-color': 'yellow',
+      'display': 'flex',
+      'flex-direction': 'row'
+    })
+    $('.tint').css({
+      'width': '50px',
+      'height': '50px',
+      'background-color': 'white',
+      'padding': '5px',
+      'border-radius': '25px'
     })
   }
 
