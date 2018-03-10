70e4fb16e0b145907d7d8a3a5c464dad502f7d21
Basel <basel.obeid9661@gmail.com>
2018-03-10 15:52:49 +0100

    cool hiding header in shop

--- src/components/genericshop/genericshop.js
+++ src/components/genericshop/genericshop.js
@@ -2,12 +2,7 @@ var $  = require('jquery')
 var _CUR = 'SEK'
 export class GenericShop {
   constructor () {
-    // this.items = {
-    //   men:[{notch: 'Title1'}, {notch: 'Title2'}, {notch: 'Title3'}],
-    //   women: {
-
-    //   }
-    // }
+    this.hiddenHeader = false
     this.items = [
                    {notch: 'Title1', image:'scarf.jpg', price:'43'+_CUR},
                    {notch: 'Title1', image:'scarf.jpg', price:'43'+_CUR},
@@ -24,26 +19,72 @@ export class GenericShop {
 
   activate(params) {
     this.gender = params.gender
-    console.log(this.gender)
   }
+
+  smoothlyHide(headerClass, cb) {
+    if (this.hiddenHeader == false){
+      this.reanimate(headerClass)
+      this.hiddenHeader = true
+      setTimeout(() => {
+        $(headerClass).addClass('smooth-hide')
+        cb()
+      }, 1000)
+    }
+  }
+
+  smoothleyShow(headerClass, cb) {
+    if(this.hiddenHeader){
+      this.reanimate(headerClass)
+      $(headerClass).addClass('smooth-show')
+      this.hiddenHeader = false
+      cb()
+    }
+  }
+
+  // reanimate(headerClass) {
+  //     var el = $(headerClass)
+  //     el.removeClass('smooth-show')
+  //     el.removeClass('smooth-hide')  
+  //     var newone = el.clone(true);
+  //     el.before(newone);
+  //     $("." + el.attr("class")).remove();
+  // }
+
+  reanimate(headerClass) {
+    var elm = document.getElementsByClassName('header')[0]
+    elm.classList.remove('smooth-show')
+    elm.classList.remove('smooth-hide')  
+    var newone = elm.cloneNode(true);
+    elm.parentNode.replaceChild(newone, elm);
+  }
+
+
+
+  removeClassIfNeeded(owner ,shown, hidden) {
+    if (this.hiddenHeader){
+      $(owner).removeClass(hidden)
+    } else {
+      $(owner).removeClass(shown)
+    }
+  }
+
 
   attached() {
     var lastScroll = 0
     var fixed = false
-    $(window).scroll(function (event) {
+    $(window).scroll((event) => {
       var _scroll = $(window).scrollTop();
-      if(_scroll < 140 && _scroll > 115) {
-        $('.header').addClass('sticky-top')
-        fixed = true
+      
+      //this.removeClassIfNeeded('.header','smooth-show', 'smooth-hide')
+      if(lastScroll < _scroll && _scroll > 120) {
+        //this.removeClassIfNeeded('.header','smooth-show', 'smooth-hide')
+        this.smoothlyHide('.header', ()=> {})
       }
 
-      if(_scroll < 115) {
-        $('.header').removeClass('sticky')
-        fixed = false
-      }
-
-      if(lastScroll > _scroll && fixed) {
-        $('.header').addClass('hidden-animated')
+      if(lastScroll > _scroll) {
+        //this.removeClassIfNeeded('.header','smooth-show', 'smooth-hide')
+        //$('.header').addClass('sticky-top')
+        this.smoothleyShow('.header', ()=> {})
       }
 
       lastScroll = _scroll
